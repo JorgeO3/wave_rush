f0018f54da1c9f9db7502d493b1de17210b39ba6
Jorge Osorio <joheosmo@gmail.com>
2025-06-20 20:20:46 -0500

    fix: handle potential error when retrieving packets from wav_decoder

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() -> Result<()> {
     let file = std::fs::File::open(INPUT)?;
     let wav_reader = WavReader::try_new(file)?;
     let mut wav_decoder = WavDecoder::try_new(wav_reader)?;
-    let mut packets = wav_decoder.packets();
+    let mut packets = wav_decoder.packets()?;
 
     while let Some(packet) = packets.next()? {
         let _packet = packet;
